8529db11ffa29730f84bdf108a9ff6bd1613866a
smartkre <smartkreshuk@gmail.com>
2025-09-17 14:13:48 +0300

    Create stm32f4xx_hal_def.h

---
+++ Core/Inc/stm32f4xx_hal_def.h
@@ -0,0 +1,50 @@
+#ifndef __STM32F4xx_HAL_DEF
+#define __STM32F4xx_HAL_DEF
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include <stddef.h>
+#include <stdint.h>
+
+/* HAL Status definitions */
+typedef enum 
+{
+  HAL_OK       = 0x00U,
+  HAL_ERROR    = 0x01U,
+  HAL_BUSY     = 0x02U,
+  HAL_TIMEOUT  = 0x03U
+} HAL_StatusTypeDef;
+
+/* RCC structures - определения которые используются в stm32f4xx_hal_rcc.h */
+typedef struct
+{
+  uint32_t OscillatorType;
+  uint32_t HSEState;
+  uint32_t HSIState;
+  uint32_t HSICalibrationValue;
+  struct {
+    uint32_t PLLState;
+    uint32_t PLLSource;
+    uint32_t PLLM;
+    uint32_t PLLN;
+    uint32_t PLLP;
+    uint32_t PLLQ;
+  } PLL;
+} RCC_OscInitTypeDef;
+
+typedef struct
+{
+  uint32_t ClockType;
+  uint32_t SYSCLKSource;
+  uint32_t AHBCLKDivider;
+  uint32_t APB1CLKDivider;
+  uint32_t APB2CLKDivider;
+} RCC_ClkInitTypeDef;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STM32F4xx_HAL_DEF */
